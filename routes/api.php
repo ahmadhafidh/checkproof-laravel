645489810ad3f50a90d41446c822a56aa603d0a7
hafidhayatullah <ahmad.hafidhayatullah@gmail.com>
2025-10-23 13:32:05 +0700

    fixed Login and Logout APIs

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,15 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 
 Route::post('/users', [UserController::class, 'store']);
+
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
